5fff50660316acc78fd21b9ddf2c26b6a96b9d68
Inez Carrillo <inezcarrillo@Inezs-MacBook-Pro.local>
2024-03-11 20:30:05 -0500

    added basic search box function

--- public/index.js
+++ public/index.js
@@ -7,3 +7,38 @@
         //track seeds
             //all of these will allow us to recommend alternatives after a search
         //all searches need a type, we'll get a 400 error if they don't
+
+const searchBox = document.getElementById('searchBox');
+const searchResults = document.getElementById('searchResults');
+
+async function search(searchQuery) {
+    //const endpoint = `https://jsonplaceholder.typicode.com/posts/${searchQuery}/comments`;
+  
+    const response = await fetch(endpoint);
+  
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+  
+    const json = await response.json();
+    return json;
+  };
+
+  async function renderResults(text) {
+    const newData = await search(text);
+  
+    searchResults.innerHTML = '';
+  
+    newData.forEach((comment) => {
+      const body = document.createElement('h3');
+      const email = document.createElement('p');
+      const hr = document.createElement('hr');
+  
+      body.textContent = comment.body;
+      email.textContent = comment.email;
+  
+      searchResults.append(body);
+      searchResults.append(email);
+      searchResults.append(hr);
+    });
+  }
