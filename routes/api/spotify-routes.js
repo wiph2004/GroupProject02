72fc2405665f032d612edc485930ae25087d436a
wiph2004 <149805523+wiph2004@users.noreply.github.com>
2024-03-12 20:00:04 -0500

    refined spotify api to be more secure through environment vairables

--- routes/api/spotify-routes.js
+++ routes/api/spotify-routes.js
@@ -2,13 +2,22 @@ const router = require('express').Router();
 const { getSpotifyAccessToken, getAlbum } = require('spotify-node-wrapper');
 const baseURL = "https://api.spotify.com/v1/me/search?market=us&q=";
 
+const spotifyApi = {
+    clientId: '86a17ba2ebc44d938ad1938d90f156e1',
+    clientSecret: '7cab5b4ff3f3436f9178428a31400340',
+    redirect_url: 'https://wiph2004.github.io/GroupProject02/'
+};
 
-router.get('/', async (res, req) => {
+router.get('/', async (req, res) => {
     try {
-    const accessToken = await getSpotifyAccessToken('86a17ba2ebc44d938ad1938d90f156e1', '7cab5b4ff3f3436f9178428a31400340', 'https://wiph2004.github.io/GroupProject02/');
+    const accessToken = await getSpotifyAccessToken(spotifyApi);
     //.then(token => console.log("AccessToken: " + accessToken));
 
-    const newSearch = await fetch(`${baseURL}${searchParams}`, {
+    const searchParams = req.body;
+    const search = searchParams.query;
+    const type = `&type=${searchParams.type}`;
+
+    const newSearch = await fetch(`${baseURL}${search}${type}`, {
         method: 'GET',
         headers: { 'Authorization': 'Bearer ' + accessToken },
     });
