7d051d7e158034cb6d8123722549b83120aa3ddf
wiph2004 <149805523+wiph2004@users.noreply.github.com>
2024-03-18 14:55:00 -0500

    spotify api now fully functional still need to test it

--- routes/api/spotify-routes.js
+++ routes/api/spotify-routes.js
@@ -1,30 +1,31 @@
 const router = require("express").Router();
 const spotifyNodeWrapper = import("spotify-node-wrapper");
 const baseURL = "https://api.spotify.com/v1/search?market=us&q=";
+const { accessToken, accessTokenExpiry, getAccessToken } = require("../../utils/helpers");
 
-let accessToken;
-let accessTokenExpiry;
-let spotifyNodeApi;
+// let accessToken;
+// let accessTokenExpiry;
+// let spotifyNodeApi;
 
-const getAccessToken = async () => {
-    return new Promise(async (resolve, reject) => {
-        spotifyNodeApi = await spotifyNodeWrapper;
-        accessToken = await spotifyNodeApi.getSpotifyAccessToken(
-            spotifyApiCreds.clientId,
-            spotifyApiCreds.clientSecret,
-            spotifyApiCreds.redirectUri
-        );
+// const getAccessToken = async () => {
+//     return new Promise(async (resolve, reject) => {
+//         spotifyNodeApi = await spotifyNodeWrapper;
+//         accessToken = await spotifyNodeApi.getSpotifyAccessToken(
+//             spotifyApiCreds.clientId,
+//             spotifyApiCreds.clientSecret,
+//             spotifyApiCreds.redirectUri
+//         );
 
-        accessTokenExpiry = Date.now() + 1 * 60 * 60 * 1000;
-        resolve(accessToken);
-    });
-} 
+//         accessTokenExpiry = Date.now() + 1 * 60 * 60 * 1000;
+//         resolve(accessToken);
+//     });
+// } 
 
-const spotifyApiCreds = {
-  clientId: "86a17ba2ebc44d938ad1938d90f156e1",
-  clientSecret: "7cab5b4ff3f3436f9178428a31400340",
-  redirectUri: "https://wiph2004.github.io/GroupProject02/",
-};
+// const spotifyApiCreds = {
+//   clientId: "86a17ba2ebc44d938ad1938d90f156e1",
+//   clientSecret: "7cab5b4ff3f3436f9178428a31400340",
+//   redirectUri: "https://wiph2004.github.io/GroupProject02/",
+// };
 
 router.post("/", async (req, res) => {
   try {
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,4 +1,32 @@
-const importSpotifyWrapper = async () => {
-    const { getSpotifyAccessToken, getAlbum } = await import('spotify-node-wrapper');
-  };
-  importSpotifyWrapper();
+const spotifyNodeWrapper = import("spotify-node-wrapper");
+
+// const importSpotifyWrapper = async () => {
+//     const { getSpotifyAccessToken, getAlbum } = await import('spotify-node-wrapper');
+//   };
+//   importSpotifyWrapper();
+
+let accessToken;
+let accessTokenExpiry;
+let spotifyNodeApi;
+
+const getAccessToken = async () => {
+    return new Promise(async (resolve, reject) => {
+        spotifyNodeApi = await spotifyNodeWrapper;
+        accessToken = await spotifyNodeApi.getSpotifyAccessToken(
+            spotifyApiCreds.clientId,
+            spotifyApiCreds.clientSecret,
+            spotifyApiCreds.redirectUri
+        );
+
+        accessTokenExpiry = Date.now() + 1 * 60 * 60 * 1000;
+        resolve(accessToken);
+    });
+} 
+
+const spotifyApiCreds = {
+  clientId: "86a17ba2ebc44d938ad1938d90f156e1",
+  clientSecret: "7cab5b4ff3f3436f9178428a31400340",
+  redirectUri: "https://wiph2004.github.io/GroupProject02/",
+};
+
+module.exports = { accessToken, accessTokenExpiry, getAccessToken };
