5bce633c5c9c0c438b7a692a2ea22ab33f73abfa
wiph2004 <149805523+wiph2004@users.noreply.github.com>
2024-03-24 12:41:57 -0500

    finalized login process and account creation

--- public/js/login.js
+++ public/js/login.js
@@ -2,8 +2,8 @@ const loginFormHandler = async (event) => {
     event.preventDefault();
   
     // Collect values from the login form
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
+    const email = document.querySelector('#login').value.trim();
+    const password = document.querySelector('#password').value.trim();
   
     if (email && password) {
       // Send a POST request to the API endpoint
@@ -15,7 +15,7 @@ const loginFormHandler = async (event) => {
   
       if (response.ok) {
         // If successful, redirect the browser to the profile page
-        document.location.replace('/profile');
+        document.location.replace('homepage');
       } else {
         alert(response.statusText);
       }
@@ -39,7 +39,7 @@ const loginFormHandler = async (event) => {
       });
   
       if (response.ok) {
-        document.location.replace('/');
+        document.location.replace('homepage');
       } else {
         alert(response.statusText);
       }
--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -2,59 +2,22 @@ const router = require('express').Router();
 const { Artist, Concert, Music, User } = require('../models');
 const withAuth = require('../utils/auth');
 
-// router.get('/', async (req, res) => {
-//   try {
-//     const projectData = await Project.findAll({
-//       include: [
-//         {
-//           model: User,
-//           attributes: ['name'],
-//         },
-//       ],
-//     });
-
-//     const projects = projectData.map((project) => project.get({ plain: true }));
-
-//     res.render('homepage', { 
-//       projects, 
-//       logged_in: req.session.logged_in 
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
-// router.get('/project/:id', async (req, res) => {
-//   try {
-//     const projectData = await Project.findByPk(req.params.id, {
-//       include: [
-//         {
-//           model: User,
-//           attributes: ['name'],
-//         },
-//       ],
-//     });
-
-//     const project = projectData.get({ plain: true });
-
-//     res.render('project', {
-//       ...project,
-//       logged_in: req.session.logged_in
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
-
 router.get('/', async (req, res) => {
   
     res.render('login');
 });
 
+router.get('/homepage', async (req, res) => {
+  if (!req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+  res.render('homepage');
+});
+
 router.get('/login', (req, res) => {
   if (req.session.logged_in) {
-    res.redirect('/homepage');
+    res.redirect('homepage');
     return;
   }
 
